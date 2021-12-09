23f33337b118dbc35de437f8d09e1f621c7df99a
niltoneapontes <niltoneapontes@gmail.com>
2021-12-09 12:03:14 -0300

    feat: add firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,14 @@ import 'package:bookbuddies/providers/location_provider.dart';
 import 'package:bookbuddies/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ChangeNotifierProvider(
       create: (context) => LocationProvider(),
